5230a0ba04d6be083c06c805fa53dd48dd57a649
Paulo Oliveira <pauloemmanuelferreira@gmail.com>
2021-12-28 15:15:23 -0300

    Função de Abrir e fechar menu parcialmente pronto

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+document.querySelector('#menu-img').addEventListener('click',()=>{
+  
+  //Mudando o tamanho do aside
+  document.querySelector('aside').classList.toggle('open');
+  document.querySelector('aside').classList.toggle('closed');
+
+  //Mudando os estados do restante
+  document.querySelector('#vet-logo').classList.toggle('none')
+  document.querySelector('#search-area').classList.toggle('none')
+  let textList = document.querySelectorAll('.menu-texts');
+
+  textList.forEach(function(item){
+    item.classList.toggle('none');
+  })
+
+  
+
+
+})
